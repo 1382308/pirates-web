70d60ecb99b12b5f0e0c75adff65fecb5cfae2b9
Carlos Guajardo <142276647+1382308@users.noreply.github.com>
2026-02-10 12:58:35 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCxicZMalP0Z2HAcnlM1AQ6olCfEanh3rU",
+  authDomain: "pirates-web-f0732.firebaseapp.com",
+  databaseURL: "https://pirates-web-f0732-default-rtdb.firebaseio.com",
+  projectId: "pirates-web-f0732",
+  storageBucket: "pirates-web-f0732.firebasestorage.app",
+  messagingSenderId: "720364197539",
+  appId: "1:720364197539:web:f0c10129859ea56023d116"
+};
